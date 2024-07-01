d58ed1d0666597e9aade4916aa1a356a168a6f8a
alexchtst-01 <acinatra@gmail.com>
2024-07-02 06:16:15 +0700

    msg : first commit

---
+++ android/app/src/main/kotlin/com/example/loglife/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loglife
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:loglife/color.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor
+      ),
+      home: const Text('data'),
+    );
+  }
+}
